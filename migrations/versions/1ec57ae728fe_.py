5802d7d4200bc1ed798b8921f0cd576ae09c1d1a
Thomas C. Hicks <paradox@pobox.com>
2017-06-08 12:07:01 -0400

    Adding a throw away change to models
    The tutorial talks about adding a throw away change to test downgrades.  It doesn't work for me, can't make the migration work because it tells me the database is not up to date. Without migrations I don't know how it is possible for the database to be up to date.  Apparently the tutorial is working from an old version of flask-migrate, need to learn about the current version and how it works (with revision, migrate, upgrade and downgrade commands).

---
+++ migrations/versions/1ec57ae728fe_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 1ec57ae728fe
+Revises: ef7d15f301ca
+Create Date: 2017-06-08 11:55:14.273774
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1ec57ae728fe'
+down_revision = 'ef7d15f301ca'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
