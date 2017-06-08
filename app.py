c40c64436350a10dc056912e910d701646aa288e
Thomas C. Hicks <paradox@pobox.com>
2017-06-08 17:36:33 -0400

    Add Blueprints (tutorial part 19)
    Put in projects/users folder to set up users as a blueprint
    Moved a bunch of stuff around (bcrypt handled by blueprint, etc.)

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
+#################
+#### Imports ####
+#################
+
 from flask import Flask, render_template, redirect, url_for, request, session,\
  flash
-# from flask.ext.sqlalchemy import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 from functools import wraps
-
-# import sqlite3
+import os
 
 app = Flask(__name__)
-bcrypt = Bcrypt(app)
 
 # config
 # before starting the server you need to define an environment variable
@@ -16,9 +16,7 @@ bcrypt = Bcrypt(app)
 # for example: export APP_SETTINGS="config.DevelopmentConfig" if you want
 # to use the development configuration.
 
-import os
 app.config.from_object(os.environ['APP_SETTINGS'])
-
 db = SQLAlchemy(app)
 
 # Import models after you create the SQLAlchemy object! - reference models.py -
@@ -26,6 +24,16 @@ db = SQLAlchemy(app)
 # before you define db.
 
 from models import *
+from project.users.views import users_blueprint
+
+
+# register our blueprints
+app.register_blueprint(users_blueprint)
+
+
+##########################
+#### helper functions ####
+##########################
 
 
 # Makes you login before you do certain things on the site
@@ -36,18 +44,17 @@ def login_required(test):
             return test(*args, **kwargs)
         else:
             flash('You need to login first.')
-            return redirect(url_for('login'))
+            return redirect(url_for('users.login'))
     return wrap
 
+
+################
+#### routes ####
+################
 
 @app.route('/')
 @login_required
 def home():
-    # g.db = connect_db()
-    # cur = g.db.execute('select * from posts')
-    # posts = [dict(title=row[0], description=row[1]) for row in \
-    # cur.fetchall()]
-    # g.db.close()
     posts = db.session.query(BlogPost).all()
     return render_template('index.html', posts=posts)
 
@@ -57,29 +64,26 @@ def welcome():
     return render_template('welcome.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password']\
-         != 'admin':
-            error = 'Invalid credentials.  Please try again.'
-        else:
-            session['logged_in'] = True
-            flash("You were just logged in")
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
-
-
-@app.route("/logout")
-@login_required
-def logout():
-    session.pop('logged_in', None)
-    flash("You were just logged out")
-    return redirect(url_for('welcome'))
-
-# def connect_db():
-#    return sqlite3.connect(app.database)
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if request.form['username'] != 'admin' or request.form['password']\
+#          != 'admin':
+#             error = 'Invalid credentials.  Please try again.'
+#         else:
+#             session['logged_in'] = True
+#             flash("You were just logged in")
+#             return redirect(url_for('home'))
+#     return render_template('login.html', error=error)
+#
+#
+# @app.route("/logout")
+# @login_required
+# def logout():
+#     session.pop('logged_in', None)
+#     flash("You were just logged out")
+#     return redirect(url_for('welcome'))
 
 
 if __name__ == '__main__':
