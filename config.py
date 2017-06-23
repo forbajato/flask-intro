f3982a7b89c86052222a481012f5a90f9ae7c0b3
Thomas C. Hicks <paradox@pobox.com>
2017-06-23 16:48:02 -0400

    Completed Part 22 - Unit testing with Flask-Testing
    Fixed up the tests, added base test case, add testing config
    Testing against an sqlite database created by the testing program
    Data added in setup from testing program
    Tests run against this test database.

--- config.py
+++ config.py
@@ -13,6 +13,13 @@ class BaseConfig(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
+class TestConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
+
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
 
--- test.py
+++ test.py
@@ -1,24 +1,42 @@
-from project import app
 import unittest
+from flask.ext.testing import TestCase
+from project import app, db
+from project.models import User, BlogPost
 
-class FlaskTestCase(unittest.TestCase):
+
+class BaseTestCase(TestCase):
+    """ A Base test case """
+
+    def create_app(self):
+        app.config.from_object('config.TestConfig')
+        return app
+
+    def setUp(self):
+        db.create_all()
+        db.session.add(BlogPost("Test post", "This is a test. Only a test."))
+        db.session.add(User("admin", "ad@min.com", "admin"))
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+
+class FlaskTestCase(BaseTestCase):
 
     # Ensure that Flask was set up correctly
     def test_index(self):
-        tester = app.test_client(self)
-        response = tester.get('/login', content_type='html/text')
+        response = self.client.get('/login', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
     # Ensure that login page loads correctly
     def test_login_page_loads(self):
-        tester = app.test_client(self)
-        response = tester.get('/login', content_type='html/text')
+        response = self.client.get('/login', content_type='html/text')
         self.assertTrue(b'Please login' in response.data)
 
     # Ensure that login behaves correctly with correct credentials
     def test_correct_login(self):
-        tester = app.test_client(self)
-        response = tester.post(
+        response = self.client.post(
             '/login',
             data=dict(username="admin", password="admin"),
             follow_redirects=True)
@@ -26,8 +44,7 @@ class FlaskTestCase(unittest.TestCase):
 
     # Ensure that login behaves correctly with incorrect credentials
     def test_incorrect_login(self):
-        tester = app.test_client(self)
-        response = tester.post(
+        response = self.client.post(
             '/login',
             data=dict(username="tom", password="tom"),
             follow_redirects=True)
@@ -35,29 +52,26 @@ class FlaskTestCase(unittest.TestCase):
 
     # Ensure that logout behaves correctly
     def test_logout(self):
-        tester = app.test_client(self)
-        tester.post(
+        self.client.post(
             '/login',
             data=dict(username="admin", password="admin"),
             follow_redirects=True
         )
-        response = tester.get('/logout',follow_redirects=True)
+        response = self.client.get('/logout',follow_redirects=True)
         self.assertIn(b'You were just logged out', response.data)
 
     # Ensure the main page requires a login
     def test_main_route_requires_login(self):
-        tester = app.test_client(self)
-        response = tester.get('/', follow_redirects=True)
+        response = self.client.get('/', follow_redirects=True)
         self.assertTrue(b'You need to login first' in response.data)
 
     # Ensure that posts show up on the main page
     def test_post_show_up(self):
-        tester = app.test_client(self)
-        response = tester.post(
+        response = self.client.post(
             '/login',
             data=dict(username="admin", password="admin"),
             follow_redirects=True)
-        self.assertIn(b'Shell test', response.data)
+        self.assertIn(b'Test post', response.data)
 
 if __name__ == '__main__':
     unittest.main()
