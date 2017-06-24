8e6e58dba1803cfa140050300449f4d7c3daacd7
Thomas C. Hicks <paradox@pobox.com>
2017-06-24 18:33:21 -0400

    Completed Part 26 - Finalize messaging
    Added messaging form (to add new messages)
    Updated model to allow for author_id

--- project/home/views.py
+++ project/home/views.py
@@ -4,8 +4,9 @@
 
 from project import db
 from project.models import BlogPost
-from flask import render_template, Blueprint
-from flask_login import login_required
+from project.home.forms import MessageForm
+from flask import render_template, Blueprint, flash, url_for, redirect, request
+from flask_login import login_required, current_user
 
 
 #################
@@ -22,11 +23,25 @@ home_blueprint = Blueprint(
 ################
 
 
-@home_blueprint.route('/')
+@home_blueprint.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    posts = db.session.query(BlogPost).all()
-    return render_template('index.html', posts=posts)
+    error = None
+    form = MessageForm(request.form)
+    if form.validate_on_submit():
+        new_message = BlogPost(
+            form.title.data,
+            form.description.data,
+            current_user.id
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        flash('New entry was successfully posted. Thanks.')
+        return redirect(url_for('home.home'))
+    else:
+        posts = db.session.query(BlogPost).all()
+        return render_template('index.html',
+                               posts=posts, form=form, error=error)
 
 
 @home_blueprint.route('/welcome')
